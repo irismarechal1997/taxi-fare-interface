a4636ee15277545aa10babea9196025032fdb64b
irismarechal1997 <137776782+irismarechal1997@users.noreply.github.com>
2023-09-08 11:48:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiaXJpc21hcmVjaGFsIiwiYSI6ImNsbWFld2x3ZDA1OGgzamw5aGpwY21qdTEifQ.ZQFWvKECgGPJohsoDMq0vg';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
